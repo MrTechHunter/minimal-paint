ee1db19d55d8095c2b6f9e668953e51cfb8f200c
MrTechHunter <mhmh28247@gmail.com>
2020-12-03 17:12:27 +0330

    Prevent WET code / Code Review

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const activeToolEl = document.getElementById('active-tool');
-const brushColorBtn = document.getElementById('brush-color');
-const brushIcon = document.getElementById('brush');
-const brushSize = document.getElementById('brush-size');
-const brushSlider = document.getElementById('brush-slider');
-const bucketColorBtn = document.getElementById('bucket-color');
-const eraser = document.getElementById('eraser');
-const clearCanvasBtn = document.getElementById('clear-canvas');
-const saveStorageBtn = document.getElementById('save-storage');
-const loadStorageBtn = document.getElementById('load-storage');
-const clearStorageBtn = document.getElementById('clear-storage');
-const downloadBtn = document.getElementById('download');
+const BRUSH_TIME = 1500;
+const activeToolEl = document.getElementById("active-tool");
+const brushColorBtn = document.getElementById("brush-color");
+const brushIcon = document.getElementById("brush");
+const brushSize = document.getElementById("brush-size");
+const brushSlider = document.getElementById("brush-slider");
+const bucketColorBtn = document.getElementById("bucket-color");
+const eraser = document.getElementById("eraser");
+const clearCanvasBtn = document.getElementById("clear-canvas");
+const saveStorageBtn = document.getElementById("save-storage");
+const loadStorageBtn = document.getElementById("load-storage");
+const clearStorageBtn = document.getElementById("clear-storage");
+const downloadBtn = document.getElementById("download");
 const { body } = document;
 
 // Global Variables
-const canvas = document.createElement('canvas');
-canvas.id = 'canvas';
-const context = canvas.getContext('2d');
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+const context = canvas.getContext("2d");
 let currentSize = 10;
-let bucketColor = '#FFFFFF';
-let currentColor = '#A51DAB';
+let bucketColor = "#FFFFFF";
+let currentColor = "#A51DAB";
 let isEraser = false;
 let isMouseDown = false;
 let drawnArray = [];
 
 // Formatting Brush Size
 function displayBrushSize() {
-  if(brushSlider.value < 10) {
+  if (brushSlider.value < 10) {
     brushSize.textContent = `0${brushSlider.value}`;
   } else {
     brushSize.textContent = brushSlider.value;
@@ -33,30 +34,30 @@ function displayBrushSize() {
 }
 
 // Setting Brush Size
-brushSlider.addEventListener('change', () => {
+brushSlider.addEventListener("change", () => {
   currentSize = brushSlider.value;
   displayBrushSize();
 });
 
 // Setting Brush Color
-brushColorBtn.addEventListener('change', () => {
+brushColorBtn.addEventListener("change", () => {
   isEraser = false;
   currentColor = `#${brushColorBtn.value}`;
 });
 
 // Setting Background Color
-bucketColorBtn.addEventListener('change', () => {
+bucketColorBtn.addEventListener("change", () => {
   bucketColor = `#${bucketColorBtn.value}`;
   createCanvas();
   restoreCanvas();
 });
 
 // Eraser
-eraser.addEventListener('click', () => {
+eraser.addEventListener("click", () => {
   isEraser = true;
-  brushIcon.style.color = 'white';
-  eraser.style.color = 'black';
-  activeToolEl.textContent = 'Eraser';
+  brushIcon.style.color = "white";
+  eraser.style.color = "black";
+  activeToolEl.textContent = "Eraser";
   currentColor = bucketColor;
   currentSize = 50;
 });
@@ -64,13 +65,18 @@ eraser.addEventListener('click', () => {
 // Switch back to Brush
 function switchToBrush() {
   isEraser = false;
-  activeToolEl.textContent = 'Brush';
-  brushIcon.style.color = 'black';
-  eraser.style.color = 'white';
+  activeToolEl.textContent = "Brush";
+  brushIcon.style.color = "black";
+  eraser.style.color = "white";
   currentColor = `#${brushColorBtn.value}`;
   currentSize = 10;
   brushSlider.value = 10;
   displayBrushSize();
+}
+
+// Prevent WET code
+function brushTimeSetTimeout(ms) {
+  setTimeout(switchToBrush, ms);
 }
 
 // Create Canvas
@@ -84,12 +90,12 @@ function createCanvas() {
 }
 
 // Clear Canvas
-clearCanvasBtn.addEventListener('click', () => {
+clearCanvasBtn.addEventListener("click", () => {
   createCanvas();
   drawnArray = [];
   // Active Tool
-  activeToolEl.textContent = 'Canvas Cleared';
-  setTimeout(switchToBrush, 1500);
+  activeToolEl.textContent = "Canvas Cleared";
+  brushTimeSetTimeout(BRUSH_TIME);
 });
 
 // Draw what is stored in DrawnArray
@@ -98,7 +104,7 @@ function restoreCanvas() {
     context.beginPath();
     context.moveTo(drawnArray[i - 1].x, drawnArray[i - 1].y);
     context.lineWidth = drawnArray[i].size;
-    context.lineCap = 'round';
+    context.lineCap = "round";
     if (drawnArray[i].eraser) {
       context.strokeStyle = bucketColor;
     } else {
@@ -132,18 +138,18 @@ function getMousePosition(event) {
 }
 
 // Mouse Down
-canvas.addEventListener('mousedown', (event) => {
+canvas.addEventListener("mousedown", (event) => {
   isMouseDown = true;
   const currentPosition = getMousePosition(event);
   context.moveTo(currentPosition.x, currentPosition.y);
   context.beginPath();
   context.lineWidth = currentSize;
-  context.lineCap = 'round';
+  context.lineCap = "round";
   context.strokeStyle = currentColor;
 });
 
 // Mouse Move
-canvas.addEventListener('mousemove', (event) => {
+canvas.addEventListener("mousemove", (event) => {
   if (isMouseDown) {
     const currentPosition = getMousePosition(event);
     context.lineTo(currentPosition.x, currentPosition.y);
@@ -153,7 +159,7 @@ canvas.addEventListener('mousemove', (event) => {
       currentPosition.y,
       currentSize,
       currentColor,
-      isEraser,
+      isEraser
     );
   } else {
     storeDrawn(undefined);
@@ -161,52 +167,51 @@ canvas.addEventListener('mousemove', (event) => {
 });
 
 // Mouse Up
-canvas.addEventListener('mouseup', () => {
+canvas.addEventListener("mouseup", () => {
   isMouseDown = false;
 });
 
 // Save to Local Storage
-saveStorageBtn.addEventListener('click', () => {
-  localStorage.setItem('savedCanvas', JSON.stringify(drawnArray));
+saveStorageBtn.addEventListener("click", () => {
+  localStorage.setItem("savedCanvas", JSON.stringify(drawnArray));
   // Active Tool
-  activeToolEl.textContent = 'Canvas Saved';
-  setTimeout(switchToBrush, 1500);
+  activeToolEl.textContent = "Canvas Saved";
+  brushTimeSetTimeout(BRUSH_TIME);
 });
 
 // Load from Local Storage
-loadStorageBtn.addEventListener('click', () => {
-  if (localStorage.getItem('savedCanvas')) {
+loadStorageBtn.addEventListener("click", () => {
+  if (localStorage.getItem("savedCanvas")) {
     drawnArray = JSON.parse(localStorage.savedCanvas);
     restoreCanvas();
-  // Active Tool
-    activeToolEl.textContent = 'Canvas Loaded';
-    setTimeout(switchToBrush, 1500);
+    // Active Tool
+    activeToolEl.textContent = "Canvas Loaded";
+    brushTimeSetTimeout(BRUSH_TIME);
   } else {
-    activeToolEl.textContent = 'No Canvas Found';
-    setTimeout(switchToBrush, 1500);
+    activeToolEl.textContent = "No Canvas Found";
+    brushTimeSetTimeout(BRUSH_TIME);
   }
-
 });
 
 // Clear Local Storage
-clearStorageBtn.addEventListener('click', () => {
-  localStorage.removeItem('savedCanvas');
+clearStorageBtn.addEventListener("click", () => {
+  localStorage.removeItem("savedCanvas");
   // Active Tool
-  activeToolEl.textContent = 'Local Storage Cleared';
-  setTimeout(switchToBrush, 1500);
+  activeToolEl.textContent = "Local Storage Cleared";
+  brushTimeSetTimeout(BRUSH_TIME);
 });
 
 // Download Image
-downloadBtn.addEventListener('click', () => {
-  downloadBtn.href = canvas.toDataURL('image/jpeg', 1.0);
-  downloadBtn.download = 'Minimal-Paint.jpg';
+downloadBtn.addEventListener("click", () => {
+  downloadBtn.href = canvas.toDataURL("image/jpeg", 1.0);
+  downloadBtn.download = "Minimal-Paint.jpg";
   // Active Tool
-  activeToolEl.textContent = 'Image File Saved';
-  setTimeout(switchToBrush, 1500);
+  activeToolEl.textContent = "Image File Saved";
+  brushTimeSetTimeout(BRUSH_TIME);
 });
 
 // Event Listener
-brushIcon.addEventListener('click', switchToBrush);
+brushIcon.addEventListener("click", switchToBrush);
 
 // On Load
 createCanvas();
